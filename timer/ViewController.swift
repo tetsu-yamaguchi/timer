88e4ade1569168193e236c7a88f052015c52a91c
MAC USER <macuser@MAC-no-MacBook-Air-2.local>
2017-03-19 16:27:16 +0900

    Error:exc_bad_instruction

--- timer/ViewController.swift
+++ timer/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var timer: Timer!
+    var timer_sec: Float = 0//型は大文字で
+    func updateTimer(timer: Timer) {
+        self.timer_sec += 0.1
+        self.timerLabel.text = String(format: "%.1f", timer_sec)
+    }
+    @IBAction func saisei(_ sender: Any) {
+        // 再生ボタンを押すとタイマー作成、始動
+        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    @IBAction func teisi(_ sender: Any) {
+        // タイマーを破棄
+        self.timer.invalidate()
+    }
 
+    @IBAction func reset(_ sender: Any) {
+        // リセットボタンを押すと、タイマーの時間を0に
+        self.timer_sec = 0
+        self.timerLabel.text = String(format: "%.1f", self.timer_sec)
+    }
+
+    @IBOutlet weak var timerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
